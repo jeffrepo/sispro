9d5579dc6dc3886e0a4e0a7ecc379e5a85ac54ef
jeffrepo <jeffer.leo93@gmail.com>
2021-03-05 07:21:09 -0600

    cambios

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'SISPRO',
+    'version': '1.0',
+    'category': 'Hidden',
+    'sequence': 6,
+    'summary': 'SISPRO',
+    'description': """
+
+""",
+    'depends': ['account','sale'],
+    'data': [
+        'views/sale_views.xml',
+        # 'views/account_view.xml',
+        # 'views/res_company_views.xml',
+        # 'views/account_invoice_view.xml',
+        # 'views/res_partner_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
